8c1683a108ba1700b92e3feb542c941e353b86db
983kk <haydyncm@gmail.com>
2026-04-05 22:33:15 +0000

    Removed .class files from showing in commits

--- run.sh
+++ run.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-
 set -e
 
-echo "Compiling..."
+echo "Cleaning old build..."
+rm -rf out
+mkdir -p out
 
+echo "Compiling..."
 javac -d out $(find src/main/java -name "*.java")
 
 echo "Running server..."
-
 java -cp out com.server.ModernServer
